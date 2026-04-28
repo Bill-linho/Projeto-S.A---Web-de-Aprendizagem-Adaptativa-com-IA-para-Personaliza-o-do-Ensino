120bcd5916410dbf7568fb1c940fe00695bfc837
VITOR SANTOS PEREIRA <vitor_pereira153@estudante.sesisenai.org.br>
2026-04-28 16:46:08 -0300

    28/04/2026 - 16:46 - V

--- src/models/userModels.js
+++ src/models/userModels.js
@@ -1,3 +1,31 @@
+import { defaults } from "pg";
 import sequelize from "../config/database";
 import { DataTypes } from "sequelize";
 
+export const users = sequelize.define('usuarios',
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        nome: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: { isEmail: true }
+        },
+        senha: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        ativo: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: true
+
+        }
+    });

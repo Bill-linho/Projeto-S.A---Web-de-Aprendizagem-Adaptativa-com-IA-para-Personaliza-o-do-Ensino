4132a63b2aa221465c09f491909f57ccefca1a7b
KAUÃ KAMMER VIEIRA <kaua_k_vieira@estudante.sesisenai.org.br>
2026-04-28 16:07:48 -0300

    28/04 16:08 K

---
+++ src/routes/user.router.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { listarUsuarios, buscarPerfil } from "../controllers/user.controller.js";
+import { autenticar } from "../middlewares/auth.middleware.js";
+
+const router = Router();
+
+router.get("/", autenticar, listarUsuarios);
+
+// Rota protegida — pegar usuário logado (perfil)
+router.get("/me", autenticar, buscarPerfil);
+
+export default router;

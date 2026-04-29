dc96f01544a5a2ec74dd840b3311032c855b72d0
Gabriel-Vieira <billlinho37@gmail.com>
2026-04-29 16:13:32 -0300

    Versão 1.1 (#1)
    * 28/04/2026 - 17:22 - G
    
    * 29/04/2026 - 16:10 - G
    
    ---------
    
    Co-authored-by: GABRIEL VIEIRA DE SOUSA <gabriel_v_de-sousa@estudante.sesisenai.org.br>

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,2 +1,36 @@
-import * as user from "../models/userModels.js"
+import * as userModel from "../models/userModels.js"
 import bcrypt from 'bcryptjs';
+
+export async function listarUsuarios(req, res) {
+    try {
+        const usuarios = await userModel.users.findAll({
+            attributes: {exclude: ["senha"]}
+        })
+
+        return res.status(200).json(usuarios)
+    } catch (error) {
+        return res.status(500).json({ error: "Erro ao buscar usuários"})
+    }
+    
+}
+
+export async function buscarPerfil(req, res) {
+    try {
+        const { id } = req.usuario
+
+        const usuarios = await userModel.users.findOne({
+            where:{ id },
+            attributes: {exclude:["senha"]}
+        })
+
+        if(!usuarios){
+            return res.status(404).json({error: "Usuário não encontrado"})
+        }
+
+        return res.status(200).json(usuarios)
+
+    } catch (error) {
+        return res.status(500).json({error: "Erro ao buscar o usuário"})
+    }
+    
+}

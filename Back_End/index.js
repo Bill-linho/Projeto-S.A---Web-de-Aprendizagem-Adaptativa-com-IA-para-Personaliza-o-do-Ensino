7b996ca09fbd13676d68eb9c033f50eb4751a31f
GABRIEL VIEIRA DE SOUSA <gabriel_v_de-sousa@estudante.sesisenai.org.br>
2026-05-05 17:21:21 -0300

    05/05/2026 - 17:35 - G

--- Back_End/index.js
+++ Back_End/index.js
@@ -4,12 +4,22 @@ import routerAuth from './src/routes/auth.router.js'
 import routerUser from './src/routes/user.router.js'
 import 'dotenv/config'
 import { autenticar } from './src/middlewares/auth.middleware.js'
+import { corsOption } from './src/config/cors.js'
+import { helmetConfig } from './src/config/helmet.js'
+import { limitGlobal , limitLocal } from './src/config/rateLimit.js'
 
 const app = express()
 app.use(express.json());
+app.use(corsOption)
+app.use(helmetConfig)
 
+<<<<<<< HEAD
 app.use('/user', routerAuth)
 app.use('/auth', autenticar, routerUser)
+=======
+app.use('/auth',limitLocal, routerAuth)
+app.use('/user',limitGlobal, autenticar, routerUser)
+>>>>>>> d67e2b2 (05/05/2026 - 17:21 - G)
 
 process.on('uncaughtException', (err) =>{
     console.error('erro não tratado', err);

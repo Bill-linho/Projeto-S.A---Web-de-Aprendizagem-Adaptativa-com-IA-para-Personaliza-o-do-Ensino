0f87b5fe706a784b0f4fb8ed80447e68467fec33
VITOR SANTOS PEREIRA <vitor_pereira153@estudante.sesisenai.org.br>
2026-05-05 14:51:18 -0300

    05/05/2026 - 14:51 - V

---
+++ Front_End/src/components/Header/header.jsx
@@ -0,0 +1,11 @@
+
+
+export default function Header() {
+
+    return (
+            <Header>
+                <h1>TITULO DA PAGINA</h1>
+                <p>Faça seu login para entrar utilizar o aplicativo</p>
+            </Header>
+    )
+}
---
+++ Front_End/src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import ProductLogin from "../components/productLogin/productLogin.jsx"
+
+export default function Login() {
+
+    return (
+        <div>
+            <ProductLogin></ProductLogin>
+        </div>
+        
+    )
+}

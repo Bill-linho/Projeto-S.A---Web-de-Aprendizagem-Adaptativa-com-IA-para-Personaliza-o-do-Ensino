b507a0ec245b3630046b06a5a16d343428a2e284
GABRIEL VIEIRA DE SOUSA <gabriel_v_de-sousa@estudante.sesisenai.org.br>
2026-05-07 16:03:01 -0300

    07/05/2026 - 16:02 - G

--- Front_End/src/App.jsx
+++ Front_End/src/App.jsx
@@ -1,10 +1,12 @@
 import Login from './pages/Login.jsx';
 import Cadastro from './pages/Cadastro.jsx';
 import { Route, Routes, Link } from 'react-router-dom';
+import Header from './components/Header/header.jsx'
 
 function App() {
   return (
     <>
+      <Header />
       
       <Routes>
         <Route path='/Login' element={<Login />} />

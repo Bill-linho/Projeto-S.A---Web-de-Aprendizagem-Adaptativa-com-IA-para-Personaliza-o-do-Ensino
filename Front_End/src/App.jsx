6a22c776e9a50f9cde719d5488612c0eba260e53
GABRIEL VIEIRA DE SOUSA <gabriel_v_de-sousa@estudante.sesisenai.org.br>
2026-05-07 16:26:42 -0300

    07/05/2026 - 16:30 - G

--- Front_End/src/App.jsx
+++ Front_End/src/App.jsx
@@ -1,5 +1,12 @@
+<<<<<<< HEAD
 import Login from './pages/Login.jsx'
 import Cadastro from './pages/Cadastro.jsx'
+=======
+import Login from './pages/Login.jsx';
+import Cadastro from './pages/Cadastro.jsx';
+import { Route, Routes} from 'react-router-dom';
+import Header from '../src/components/Header/header.jsx'
+>>>>>>> 0417c09 (07/05/2026 - 16:26 - G)
 
 function App() {
   return (
--- Front_End/src/pages/Login.jsx
+++ Front_End/src/pages/Login.jsx
@@ -1,11 +1,23 @@
-import ProductLogin from "../components/productLogin/productLogin.jsx"
+import Input from '../components/input/input.jsx'
+import '../components/style/Login.css'
+import Button from '../components/Button/Button.jsx'
+import { useNavigate } from 'react-router-dom'
 
 export default function Login() {
+    const navigate = useNavigate()
 
     return (
         <div>
-            <ProductLogin/>
+            <div>
+                <h2>Faça seu login para entrar na plataforma</h2>
+                <div>
+                    <Input placeholder="Email" type="email" />
+                    <Input placeholder="Senha" type="password" />
+                </div>
+                <Button>Entrar</Button>
+                <br/>
+                <Button onClick={() => navigate('/Cadastro')}>Criar conta</Button>
+            </div>
         </div>
-        
     )
 }

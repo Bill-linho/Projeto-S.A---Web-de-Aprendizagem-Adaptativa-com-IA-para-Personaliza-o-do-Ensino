7ed13daca71a2b6c7d6412cdf73802a7854c3245
GABRIEL VIEIRA DE SOUSA <gabriel_v_de-sousa@estudante.sesisenai.org.br>
2026-05-07 17:45:12 -0300

    07/05/2026 - 17:45 - G

--- Back_End/src/config/cors.js
+++ Back_End/src/config/cors.js
@@ -1,7 +1,7 @@
 import cors from 'cors'
 
 export const corsOption = cors({
-    origin: 'http://localhost:5174/',
+    origin: 'http://localhost:5173',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 })
--- Front_End/src/pages/Cadastro.jsx
+++ Front_End/src/pages/Cadastro.jsx
@@ -2,20 +2,49 @@ import '../components/style/Cadastro.css'
 import Button from '../components/Button/Button.jsx'
 import Input from '../components/input/input.jsx'
 import { useNavigate } from 'react-router-dom'
+import { useState} from 'react'
 
-export default function RegisterCard() {
+import { api } from '../services/api.js'
+
+export default function Cadastrar() {
     const navigate = useNavigate()
+
+    const [nome, setNome] = useState('')
+    const [senha, setSenha] = useState('')
+    const [email, setEmail] = useState('')
+
+    async function cadastrarUsuario() {
+        try{
+            const response = await api.post(
+                '/auth/cadastro',
+                {
+                    nome,
+                    email,
+                    senha
+                }
+            )
+            console.log(response.data)
+
+            alert('Usuário criado')
+
+            navigate('/Login')
+        }catch(Vitor){
+            console.log(Vitor)
+
+            alert('Erro ao cadastrar')
+        }
+    }
 
     return (
         <div>
             <div>
                 <h2>Faça seu cadastro para utilizar a plataforma</h2>
                 <div>
-                    <Input placeholder="Nome" type="text" />
-                    <Input placeholder="Email" type="email" />
-                    <Input placeholder="Senha" type="password" />
+                    <Input placeholder="Nome" type="text" onChange={(e)=> setNome(e.target.value)} />
+                    <Input placeholder="Email" type="email" onChange={(e)=> setEmail(e.target.value)}/>
+                    <Input placeholder="Senha" type="password" onChange={(e)=> setSenha(e.target.value)}/>
                 </div>
-                <Button>Cadastrar</Button>
+                <Button onClick={cadastrarUsuario}>Cadastrar</Button>
                 <Button onClick={() => navigate('/Login')}>Voltar</Button>
             </div>
         </div>

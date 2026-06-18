9ab01704fbbf16ab9f497695bdc4eb35224e6de9
Gabriel-Vieira <billlinho37@gmail.com>
2026-06-18 16:56:36 -0300

    18/06/2026 - 16:55 - V (#17)
    Co-authored-by: VITOR SANTOS PEREIRA <vitor_pereira153@estudante.sesisenai.org.br>

--- Front_End/src/components/Logo/roboManeiro.jsx
+++ Front_End/src/components/Logo/roboManeiro.jsx
@@ -1,7 +1,15 @@
 import logo from '../../assets/Robô maneiro sem fundo.png'
 
-export default function logoRobo(){
-    return(
-        <img src={logo} alt="logo" style={{height:'130px'}}/>
-    )
+export default function LogoRobo({ height = '95px', width = 'auto', padding = '0px'}) {
+  return (
+    <img
+      src={logo}
+      alt="logo"
+      style={{
+        height,
+        width,
+        padding
+      }}
+    />
+  );
 }
